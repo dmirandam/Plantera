646fac7b4e390ff5b8fc95b150eb9db6dfbe73b8
U-dot <71671790+U-dot@users.noreply.github.com>
2021-04-17 15:07:21 -0500

    Implementado lo que hizo Miranda

--- Code/module1.py
+++ Code/module1.py
@@ -5,39 +5,49 @@ Created on Thu Apr 15 17:10:16 2021
 @author: mbotello
 """
 
-class plant:
-    #Default constructor
-    def _init_(self):
+class Plant:
+
+    def check(self):
+        print("exists")
+    #Esto es para ofrecer un demo
+    def __init__(self):
+        self.name = "Rose"
         self.frecuencyWatering = 168 #Hours in a week
         self.lastWatering = 24 #a day ago
         if self.frecuencyWatering < self.lastWatering:
             self.needWatering = True
         else: self.needWatering = False
-        self.doneWatering=False
-        
-    #Parametrized constructor
-    def _init_(self, f,s,b):
-        self.frecuencyWatering = f
-        self.lastWatering = s
-        if self.frecuencyWatering < self.lastWatering:
-            self.needWatering = True
-        else: self.needWatering = False
         self.doneWatering = False
-        
+
+    #Parametrized constructor
+    def __init__(self, n, f,s):
+        self.name = n
+        self.frequencyWatering = int(f)
+        self.lastWatering = int(s)
+        if self.frequencyWatering < self.lastWatering:
+            self.needWatering = True
+        else:
+            self.needWatering = False #Para notificar una única vez
+            self.doneWatering = False #Para diferenciar el tiempo de la notificación al de la regada
+
     #Llamado cada hora o cuando se riega
+
     def updateLastWatering(self):
         if self.doneWatering == False:
-            self.lastWatering += 1
             if self.frequencyWatering < self.lastWatering:
-                self.needWatering = True 
-                #Preguntar si ya es true sería un paso innecesario
-            if self.needWatering:
-                self.notif("Agua",)
-        #Si se acaba de regar
+                print("         needWatering")
+                if self.needWatering == False:
+                    #self.notif("Agua")
+                    self.needWatering = True
+                self.needWatering == True#Extra, borrar
+            self.lastWatering += 1
+        #Si ya se regó
         else:
             self.lastWatering = 0
-         
-    #Notificaciones, se organizan en pilas 
+            self.doneWatering = False
+            self.needWatering = False
+
+    #Notificaciones, se organizan en pilas
     #def notif(str,num):
-        
+
         
