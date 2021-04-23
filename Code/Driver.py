0516a1e1e6ff602d5e7a3026c8219a17ae27080e
U-dot <71671790+U-dot@users.noreply.github.com>
2021-04-22 23:39:48 -0500

    Parámetros en clase plant
    Ahora la clase plant tiene cuantos parámetros se quiera. Ya es funcional

--- Code/Driver.py
+++ Code/Driver.py
@@ -1,32 +1,54 @@
 import module1
 import time
-
+#Ahora se pueden manejar cuántos parámetros se quiera por planta.
+#Los parámetros se guardan en una lista
+#Cada paámetro es una lista que contiene a los siguientes
+#    param[i] es el parámetro i
+#    param[i][0] <- nombre del parámetro, nueva variable
+#    param[i][1] <- frequencyWatering
+#    param[i][2] <- lastWatering
+#    param[i][3] <- needWatering
+#    param[i][4] <- doneWatering
 
 dem = int(input("Quieres un DEMO (0) o quieres comenzar de una (1)? "))
 
-if dem == 0:
-    print("Creamos una nueva planta llamada Rose que se riega cada 4 horas y se regó hace 3 horas")
-    rose = module1.Plant("Rose",4,3)
-elif dem == 1:
+if dem == 0:#DEMO
+    print("Creamos una nueva planta llamada Rose ",
+          "con un solo parámetro: Agua 5 3")
+    plant1 = module1.Plant("Rose", 2,
+                           [["Agua", 5, 3, False, False],
+                            ["Tierra", 5, 3, False, False]])
+elif dem == 1:#Ingresar datos uno por uno
+    #Me parece mejor pasar la info completa a la clase
+    #Con eso se puede pasar fácilmente la info en un archivo
     print("Hola, vamos a indexar una nueva planta")
-    nameNP = input("¿Cómo se llama la nueva planta? ")
 
-    #numberNP = input("Cuántos parámetros tiene la nueva planta? ")#Se implementa después
-    param1NP = input("Cómo se llama el parámetro 1? ")#Se implementa después
-
-    frequencyNP = input("Cada cuántas horas quieres llevar a cabo " + param1NP)
-    lastNP = input("Cuándo fue la última vez que se llevó a cabo" + param1NP)
-    rose = module1.Plant(nameNP,frequencyNP,lastNP)
-
+    namePlant = input("¿Cómo se llama la nueva planta? ")
+    numParam = int(input("Cuántos parámetros tiene la nueva planta? "))
+    paramPlant = [] #Va a guardar todos los parámetros de la planta
+    
+    #Guarda cada parámetro
+    for i in range (numParam):
+        nameParam = input("Cómo se llama el parámetro " + str(i) + "? ")
+        frequencyParam = int(input("Cada cuántas horas se lleva a cabo " + nameParam + "? "))
+        lastParam = int(input("Cuándo fue la última vez que se llevó a cabo " + nameParam +"? "))
+        
+        #Lo que antes era: name, frequencyWatering, lastWatering, needWatering, doneWatering
+        paramPlant.append( [nameParam, frequencyParam, lastParam, frequencyParam <= lastParam, False])
+    
+    plant1 = module1.Plant(namePlant,numParam, paramPlant)
+    
+    
 ####---------> Interacción paso de tiempo rápido
 
 for hour in range(24):
     print(hour)
-    #Implementando lo de Miranda, después se implementa la cola
-    #Se debe hacer lo mismo con cada parámetro
-    if(rose.needWatering): #Cada parámetro de cada planta
-        n = int(input("Regar a Rose: Lo hago despues (0) Ya lo hice (1):"))
-        if n==1: rose.doneWatering = True
-    rose.updateLastWatering()
+
+    for i in range(plant1.numberParam):
+        if(plant1.param[i][3]): #Cada parámetro de cada planta
+            n = int(input(str(plant1.param[i][0])+" a " + plant1.name + 
+                          ": Lo hago despues (0) Ya lo hice (1):"))
+            if n==1: plant1.param[i][4] = True
+        plant1.updateParameteri(i)
     time.sleep(1)
     # Pasa un segundo que contamos como una hora
--- Code/module1.py
+++ Code/module1.py
@@ -1,37 +1,45 @@
 # -*- coding: utf-8 -*-
 
+
+#Exactamente igual, solo se cambiaron los nombres
+#    param[i] es el parámetro i
+#    param[i][0] <- nombre del parámetro, nueva variable
+#    param[i][1] <- frequencyWatering
+#    param[i][2] <- lastWatering
+#    param[i][3] <- needWatering
+#    param[i][4] <- doneWatering
+
 class Plant:
 
     def check(self): #Revisar si se creo algo
         print("exists")
-
+    
     #Parametrized constructor
-    def __init__(self, n, f,s):
-        self.name = n 
-        self.frequencyWatering = int(f)
-        self.lastWatering = int(s)
-        if self.frequencyWatering < self.lastWatering:
-            self.needWatering = True
-        else:
-            self.needWatering = False #Para notificar una única vez
-        self.doneWatering = False #Para diferenciar el tiempo de la entrada y la salida de la cola
-
-    def updateLastWatering(self):
-        if self.doneWatering == False:
-            if self.frequencyWatering <= self.lastWatering: #Entonces sí se necesita agua
-                print("         needWatering")
-                if self.needWatering == False:
+    def __init__(self, n, np, p):
+        self.name = n
+        self.numberParam = np
+        #param es una lista que contiene a cada parámetro
+        #Cada parámetro es una lista que contiene la info Name, frequency, last, need, done
+        #Lo que antes era FrequencyWatering, lastWatering, needWatering, doneWatering
+        self.param = p
+        
+    def updateParameteri(self,i):
+        #Exactamente igual a updateWatering, solo se cambiaron los nombres correspondientes
+        if self.param[i][4] == False:
+            if self.param[i][1] <= self.param[i][2]-1: #Entonces sí se necesita agua
+                print("         need"+str(self.param[i][0])+" next hour")
+                if self.param[i][3] == False:
                     #self.notif("Agua")
-                    self.needWatering = True
-            self.lastWatering += 1
-        #Si ya se regó
-        else: #doneWaterinf==True
-            self.lastWatering = 0
-            self.doneWatering = False
-            self.needWatering = False
+                    self.param[i][3] = True
+            self.param[i][2] += 1
+        
+        #Si se acaba de regar
+        else: 
+            self.param[i][2] = 0
+            self.param[i][4] = False
+            self.param[i][3] = False
 
     #Notificaciones, se organizan en colas
     #def notif(str,num):
 
-    # CRUD se implementa por fuera
-        
+    # CRUD 
