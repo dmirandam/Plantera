06d04b5e20f4a586a82a72553c36a9db7b6e62f1
jyarac <82291106+jyarac@users.noreply.github.com>
2021-04-26 22:02:22 -0500

    Update module1.py

--- Code/module1.py
+++ Code/module1.py
@@ -22,16 +22,43 @@ class Plant:
         #Cada parámetro es una lista que contiene la info Name, frequency, last, need, done
         #Lo que antes era FrequencyWatering, lastWatering, needWatering, doneWatering
         self.param = p
-        
+        self.cola = {}
+
+
     def updateParameteri(self,i):
         #Exactamente igual a updateWatering, solo se cambiaron los nombres correspondientes
         if self.param[i][4] == False:
             if self.param[i][1]-3 < self.param[i][2]: #Entonces sí se necesita agua
-                print("         need"+str(self.param[i][0])+" next hour")
                 if self.param[i][3] == False:
-                    #self.notif("Agua")
+                    self.cola[self.param[i][0]] = 0
                     self.param[i][3] = True
             self.param[i][2] += 1
+        
+        #Si se acaba de regar
+        else: 
+            self.param[i][2] = 0
+            self.param[i][4] = False
+            self.param[i][3] = False
+
+    #Notificaciones, se organizan en colas
+    
+    def notif(self):
+      print ("              ***NOTIFICACIONES***")
+      for i in self.cola:
+        if self.cola[i] == 0:
+          print("debes", i, "a", self.name)
+        else:
+          print("debes",i, "a", self.name, "tuviste que hacerlo hace:", self.cola[i], "horas")
+        self.cola[i] += 1
+    
+    def Desnotif(self,a):
+      for i in self.cola:
+        if a == i:
+          del self.cola[i]
+          print("           Has hecho",a," a",self.name)
+          break
+
+
         
         #Si se acaba de regar
         else: 
